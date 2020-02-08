12620a79154f79b3b2cfbc18d6e4788cd340acb4
박준희 <tekaaluk9@bagjunhuiui-MacBookPro.local>
2020-02-08 17:15:30 +0900

    fix MemberDetail

--- src/components/auth/MemberDetail.js
+++ src/components/auth/MemberDetail.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import {connect} from 'react-redux'
+import { firestoreConnect } from "react-redux-firebase";
+import { compose } from 'redux'
 
 class MemberDetail extends Component {
 
@@ -8,9 +10,9 @@ class MemberDetail extends Component {
     }
 
     render() {
-
-        const auth = this.props.auth
-        const user = this.props.user
+        const userList = this.props.users
+        const id = this.props.id
+        const user = userList? userList.find(user => user.id === id) : {}
 
         return (
             <div className='container Site-content'>
@@ -19,33 +21,27 @@ class MemberDetail extends Component {
                     <tbody>
                         <tr>
                             <td>이메일</td>
-                            <td>{auth.email}</td>
-                            <td><span className="btn brown">변경</span></td>
+                            <td>{user.email}</td>
                         </tr>
                         <tr>
                             <td>이름</td>
                             <td>{user.name}</td>
-                            <td><span className="btn brown">변경</span></td>
                         </tr>
                         <tr>
                             <td>주소1(+우편번호)</td>
                             <td>{user.address}(우편번호 {user.postnum})</td>
-                            <td><span className="btn brown">변경</span></td>
                         </tr>
                         <tr>
                             <td>주소2</td>
                             <td>{user.building}</td>
-                            <td><span className="btn brown">변경</span></td>
                         </tr>
                         <tr>
                             <td>연락처</td>
                             <td>{user.contact}</td>
-                            <td><span className="btn brown">변경</span></td>
                         </tr>
                         <tr>
                             <td>상태</td>
                             <td>{user.status}</td>
-                            <td><span className="btn brown">변경</span></td>
                         </tr>
                     </tbody>
                 </table>
@@ -58,11 +54,13 @@ class MemberDetail extends Component {
 }
 
 const mapStateToProps = (state, ownProps) => {
-
     return {
-        auth: state.auth.auth.find(member => member.uid === ownProps.match.params.id),
-        user: state.auth.user.find(member => member.uid === ownProps.match.params.id)
+        users: state.firestore.ordered.users,
+        id: ownProps.match.params.id
     }
 }
 
-export default connect(mapStateToProps)(MemberDetail)
+export default compose(
+    connect(mapStateToProps),
+    firestoreConnect([{collection:'users'}])
+)(MemberDetail)
