263b73e17ce44f9612c4f3e02a74df9b7c6ac2ce
박준희 <tekaaluk9@bagjunhuiui-MacBookPro.local>
2020-03-30 19:35:30 +0900

    change CreateRecipe to functional component

--- src/components/recipes/CreateRecipe.js
+++ src/components/recipes/CreateRecipe.js
@@ -1,94 +1,89 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import RecipeContent from './RecipeContent'
 
-class CreateRecipe extends Component {
-    state = {
+const CreateRecipe = () => {
+
+    const [recipe, setRecipe] = useState({
         name: '',
         time: 0,
         ingredients: [],
         difficulty: '',
         img: '',
-        content: [''],
-        contentImg: ['']
+        content: [],
+        contentImg: []
+    })
+
+    const handleChange = (e) => {
+        setRecipe({...recipe, [e.target.id]: e.target.value})
+        console.log(recipe)
     }
 
-    handleChange = (e) => {
-        this.setState({
-            [e.target.id]: e.target.value
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(recipe)
+    }
+
+    const handleIngredientChange = (e) => {
+        setRecipe({...recipe, 
+        //정규식으로 띄어쓰기 상관없게 만들기
+            [e.target.id]: e.target.value.split(', ')})
+    }
+
+    const addContent = (e) => {
+        e.preventDefault()
+        setRecipe({...recipe,
+            content: [...recipe.content, ''],
+            contentImg: [...recipe.contentImg, '']
         })
     }
 
-    handleIngredientChange = (e) => {
-        this.setState({
-            [e.target.id]: e.target.value.split(', ')
-        })
-    }
-
-    handleSubmit = (e) => {
+    const handleImageButtonClick = (e) => {
         e.preventDefault()
-        console.log('submit button clicked')
-        console.log(this.state);
-        
+        console.log('handle image button clicked')
     }
 
-    handleImageButtonClick = (e) => {
-        e.preventDefault()
-        console.log('image upload')
-    }
-
-    addContent = (e) => {
-        e.preventDefault()
-        this.setState({
-            content: [...this.state.content, ''],
-            contentImg: [...this.state.contentImg, '']
-        })
-        console.log('added new step')
-    }
-
-    render() {
-        return (
-            <div className="container Site-content">
-            <form onSubmit={this.handleSubmit}>
+    return ( 
+        <div className="container Site-content">
+            <form onSubmit={handleSubmit}>
                     <h4 className="grey-text text-darken-1">레시피 등록</h4>
                     <div className="row">
                         <div className="col s12 l6">
-                            <img className="responsive-img" src={this.state.img} alt=""/>
+                            <img className="responsive-img" src={recipe.img} alt=""/>
                         </div>
                         <div className="col s12 l6">
                             <div className="input-field">
                                 <label htmlFor="name">레시피명</label>
-                                <input type="text" id="name" onChange={this.handleChange}/>
+                                <input type="text" id="name" onChange={handleChange}/>
                             </div>
                             <div className="input-field">
                                 <label htmlFor="time">시간</label>
-                                <input type="number" id="time" onChange={this.handleChange}/>
+                                <input type="number" id="time" onChange={handleChange}/>
                             </div>
                             <div className="input-field">
                                 <label htmlFor="ingredients">재료</label>
-                                <input type="text" id="ingredients" onChange={this.handleIngredientChange}/>
+                                <input type="text" id="ingredients" onChange={handleIngredientChange}/>
                             </div>
                             <div className="input-field">
                                 <label htmlFor="difficulty">난이도</label>
-                                <input type="text" id="difficulty" onChange={this.handleChange}/>
+                                <input type="text" id="difficulty" onChange={handleChange}/>
                             </div>
                             <div className="input-field">
                                 <label htmlFor="img">이미지</label>
-                                <input type="text" id="img" onChange={this.handleChange}/>
-                                <button onClick={this.handleImageButtonClick} type="button" className="btn brown lighten-2">이미지 업로드</button>
+                                <input type="text" id="img" onChange={handleChange}/>
+                                <button onClick={handleImageButtonClick} type="button" className="btn brown lighten-2">이미지 업로드</button>
                             </div>
                         </div>
                     </div>
                     <div className="input-field">
-                        {this.state.content.map((step, idx) => <RecipeContent key={idx}/>)}
+                        {recipe.content.map((step, idx) => <RecipeContent step={idx} key={idx}/>)}
                     </div>
                     <div className="input-field center">
-                        <button className="btn brown lighten-2" style={{marginRight:"4px"}} onClick={this.addContent}>내용 추가</button>
+                        <button className="btn brown lighten-2" style={{marginRight:"4px"}} onClick={addContent}>내용 추가</button>
                         <button className="btn brown">레시피 등록</button>
                     </div>
                 </form>
             </div>
-        )
-    }
+    );
 }
-
-export default CreateRecipe
+ 
+export default CreateRecipe;
--- src/components/recipes/RecipeContent.js
+++ src/components/recipes/RecipeContent.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const RecipeContent = () => {
+const RecipeContent = (step) => {
     return (
         <div className="row">
             <div className="col s12 l4 input-field">
@@ -9,7 +9,7 @@ const RecipeContent = () => {
                 <h4>image</h4>
             </div>
             <div className="col s12 l8 input-field valign-wrapper">
-                <textarea className="materialize-textarea"></textarea>
+                <textarea className="materialize-textarea" id={'step'+step}></textarea>
             </div>
         </div>
     )
