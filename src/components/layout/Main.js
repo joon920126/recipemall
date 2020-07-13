02b40c4a33b4c5988a3f05c61e846faa688fd29c
박준희 <tekaaluk9@bagjunhuiui-MacBookPro.local>
2020-07-13 22:14:33 +0900

    add recommendation display on main page

--- src/components/layout/Main.js
+++ src/components/layout/Main.js
@@ -1,7 +1,30 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Search from './Search'
+import firebase from '../../config/fbconfig'
+import ProductButton from '../products/ProductButton'
+import RecipeButton from '../recipes/RecipeButton'
+import RecipeButtonAlt from '../recipes/RecipeButtonAlt'
 
-const Main = () => {
+const Main = (props) => {
+    const [reccRecipe, setReccRecipe] = useState([])
+    const [reccProduct, setReccProduct] = useState([])
+    const [favorite, setFavorite] = useState([])
+    const isAdmin = firebase.auth().currentUser && (firebase.auth().currentUser.uid==='XlIC5HDHQIOYDc9wILQokNfhzFA2')
+
+
+    useEffect(() => {
+        firebase.firestore().collection('recommendation').doc('recommendation').get().then((doc) => {
+            setReccProduct(doc.data().product)
+            setReccRecipe(doc.data().recipe)
+        })
+    }, [])
+
+    useEffect(() => {
+        firebase.auth().currentUser && firebase.firestore().collection('users').doc(firebase.auth().currentUser.uid).get().then((doc) => {
+            setFavorite(doc.data().favorite)
+        })
+    })
+
     return (
         <div className='Site-content'>
             <div className='container'>
@@ -20,8 +43,18 @@ const Main = () => {
             </div>
             <div className='container'>
                 <h5>추천상품</h5>
+                <div className='row'>
+                    {reccProduct.map((item) => <ProductButton product={item} key={'product'+item.id} isAdmin={isAdmin} isRecc={true}/>)}
+                </div>
                 <h5>추천레시피</h5>
-                <h5>신상품</h5>
+                <div className='row'>
+                    {reccRecipe.map((item) => {
+                        return favorite.some((fav) => fav.id===item.id) ?
+                            <RecipeButtonAlt recipe={item} key={'recipe'+item.id} isAdmin={isAdmin} isRecc={true}/>:
+                            <RecipeButton recipe={item} key={'recipe'+item.id} isAdmin={isAdmin} isRecc={true}/>
+                    })}
+                </div>
+                {/* <h5>신상품</h5> */}
             </div>
         </div>
     )
